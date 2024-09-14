dbcb31cc0a423b40c4e0965dfacc2246c40f4558
RikkaRin <100074184+Alukym@users.noreply.github.com>
2024-09-14 23:15:16 +0800

    use info in build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,17 @@ REPO="git@github.com:Alukym/Melt-stuff.git"
 DIR="build_scripts"
 BUILD_SCRIPT="build_kernel.sh"
 
+info() {
+    echo -e "\e[32minfo: $1\e[0m"
+}
+
 clone_repo() {
-    echo "Repo not found, cloning..."
+    info "Repo not found, cloning..."
     git clone "$REPO" "$DIR"
 }
 
 update_repo() {
-    echo "Pulling latest changes..."
+    info "Pulling latest changes..."
     cd "$DIR" || exit 1
 
     # git clean -fd
